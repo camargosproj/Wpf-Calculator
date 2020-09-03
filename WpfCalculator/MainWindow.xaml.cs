97aa38b9c1d93ac02748ce4192ea7b2473816563
Marcos Camargo <marcoscamargo@outlook.com>
2020-09-02 23:00:27 -0300

    Added Subtraction, Division, and Multiplication. There're a few bugs that are needed to be fixed as if I start adding, I can't do other operations.

--- WpfCalculator/MainWindow.xaml.cs
+++ WpfCalculator/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ namespace WpfCalculator
     public partial class MainWindow : Window
     {
         string zero = "0";
-        public string one = "1";
+        string one = "1";
         string two = "2";
         string three = "3";
         string four = "4";
@@ -18,8 +18,12 @@ namespace WpfCalculator
         string seven = "7";
         string eight = "8";
         string nine = "9";
-        int sum = 0;
+        double sum = 0;
         bool isAddition = false;
+        bool isSubtraction = false;
+        bool isMultiplication = false;
+        bool isDivision = false;
+        double result = 0;
 
         public MainWindow()
         {
@@ -29,76 +33,110 @@ namespace WpfCalculator
         {
            
             textBox.Text += zero;
-            sum += Convert.ToInt32(textBox.Text);
-           
-
+          
         }
         private void Click_One(object sender, RoutedEventArgs e)
         {
             
-            textBox.Text += BtnOne.Content;
-            sum += Convert.ToInt32(textBox.Text);
-            
+            textBox.Text += one;
+        
+
         }
         private void Click_Two(object sender, RoutedEventArgs e)
         {
             textBox.Text += two;
-            sum += Convert.ToInt32(textBox.Text);
         }
         private void Click_Three(object sender, RoutedEventArgs e)
         {
             textBox.Text += three;
-            sum += Convert.ToInt32(textBox.Text);
-
-
+         
 
         }
         private void Click_Four(object sender, RoutedEventArgs e)
         {
             textBox.Text += four;
-            sum += Convert.ToInt32(textBox.Text);
+          
 
         }
         private void Click_Five(object sender, RoutedEventArgs e)
         {
             textBox.Text += five;
-            sum += Convert.ToInt32(textBox.Text);
+            
 
         }
         private void Click_Six(object sender, RoutedEventArgs e)
         {
             textBox.Text += six;
-            sum += Convert.ToInt32(textBox.Text);
+          
         }
         private void Click_Seven(object sender, RoutedEventArgs e)
         {
             textBox.Text += seven;
-            sum += Convert.ToInt32(textBox.Text);
 
         }
         private void Click_Eight(object sender, RoutedEventArgs e)
         {
             textBox.Text += eight;
-            sum += Convert.ToInt32(textBox.Text);
 
         }
 
         private void Click_Nine(object sender, RoutedEventArgs e)
         {
             textBox.Text += nine;
-            sum += Convert.ToInt32(textBox.Text);
 
         }
         private void Click_add(object sender, RoutedEventArgs e)
         {
+            sum += Convert.ToDouble(textBox.Text);
             textBox.Text = "";
             isAddition = true;
+        } 
+        private void Click_Sub(object sender, RoutedEventArgs e)
+        {
+            sum += Convert.ToDouble(textBox.Text);
+            textBox.Text = "";
+            isSubtraction = true;
+        }
+        private void Click_Mul(object sender, RoutedEventArgs e)
+        {
+            sum += Convert.ToDouble(textBox.Text);
+            textBox.Text = "";
+            isMultiplication = true;
+        }
+        private void Click_Div(object sender, RoutedEventArgs e)
+        {
+            sum += Convert.ToDouble(textBox.Text);
+            textBox.Text = "";
+            isDivision = true;
         }
         private void Click_Equal(object sender, RoutedEventArgs e)
         {
 
-            textBox.Text = sum.ToString();
-          
+
+            if (isAddition == true)
+            {
+                sum += Convert.ToDouble(textBox.Text);
+                
+            }
+            else if (isSubtraction == true)
+            {
+                sum -= Convert.ToDouble(textBox.Text);
+            }
+            else if (isMultiplication == true)
+            {
+                sum *= Convert.ToDouble(textBox.Text);
+
+            }
+            else if (isDivision == true)
+            {
+                sum /= Convert.ToDouble(textBox.Text);
+
+            }
+            result = sum;
+            textBox.Text = result.ToString();
+            sum = 0;
+            
+
         }
         private void Click_Del(object sender, RoutedEventArgs e)
         {
